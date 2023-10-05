ecc36c04b926feb567753a95fe817339d106588f
farkas93 <zsombor.kalotay@gmx.ch>
2023-10-05 22:50:01 +0200

    remove minor warning

--- src/layers/layer.rs
+++ src/layers/layer.rs
@@ -6,7 +6,7 @@ pub trait Layer {
     fn forward(&mut self, x: &DMatrix<f64>) -> &DMatrix<f64>;
     fn backward(&mut self, cache: &mut BackpropCache);
 
-    fn update(&mut self, learning_rate: f64) {
+    fn update(&mut self, _learning_rate: f64) {
         // Activation functions do not have anything to update
         return;
     }
